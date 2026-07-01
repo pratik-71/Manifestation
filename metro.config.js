350341a8cf345f8a5bfef320f8491b6da7e81882
pratik-71 <pratikdabhade66344@gmail.com>
2026-07-01 22:59:21 +0530

    chore: add metro.config.js to mock stream module and fix supabase build

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,16 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'stream') {
+    return {
+      type: 'empty',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
